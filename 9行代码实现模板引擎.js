fa87142eafc9f3e25f93af528a84412f0a6055e8
wusfen <404315887@qq.com>
2017-06-13 17:15:30 +0800

    Create 9行代码实现模板引擎.js

--- 9行代码实现模板引擎.js
+++ 9行代码实现模板引擎.js
@@ -4,7 +4,7 @@ Tpl = function(tpl, data) {
     var fn = tpl.replace(/&lt;/g, '<').replace(/&gt;/g, '>') //    转义 <>
         .replace(/(<%=)([\s\S]*?)(%>)/g, '$1_html_+= ($2)\n$3') // <%= %>  [\s\S]允许换行
         .replace(/(<%)(?!=)([\s\S]*?)(%>)/g, '$1\n\t$2\n$3') // <% js code %>  (?!=)不要匹配到<%= %>
-        .replace(/(^|%>|%>)([\s\S]*?)(<%=|<%|$)/g, function($, $1, $2, $3) { // 边界符外的html, html 中的(\|"|\r|\n) 要转义
+        .replace(/(^|%>|%>)([\s\S]*?)(<%=|<%|$)/g, function($, $1, $2, $3) { // 边界符外的html, html中的(\|"|\r|\n)要转义
             return '_html_+= "' + $2.replace(/\\/g, '\\\\').replace(/"/g, '\\"').replace(/\r?\n/g, '\\n') + '"\n'
         });
     return (fn = Function('data', 'with(data||{}){\nvar _html_=""\n' + fn + '\nreturn _html_\n}')), data ? fn(data) : fn
@@ -25,6 +25,34 @@ var render = Tpl(String(function(){/*
   <% } %>
 </ul>
 */}).match(/^.*?\*([\s\S]*)\*/)[1]);
+
+
+// 以上模板实际上编译成了以下代码(render函数)
+/*var render = (function(data) {
+    with (data || {}) {
+        var _html_ = ""
+        _html_ += "\n<ul>\n  "
+        
+        for (var i = 0; i < n; i++) {
+            _html_ += "\n  <li>\n    <span> "
+            _html_ += (i)
+            _html_ += " </span>\n    "
+            
+            if (i % 2 == 0) {
+                _html_ += "\n    <span> even </span>\n    "
+            
+            }
+            _html_ += "\n  </li>\n  "
+        
+        }
+        _html_ += "\n</ul>\n"
+        
+        return _html_
+    }
+})*/
+
+
+// 使用数据执行渲染
 render({n:10})
 
 
